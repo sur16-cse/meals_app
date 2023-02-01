3aeef603317bf7ad9ab3958abc9de963783a5d40
Surbhi <surbhikumaridav@gmail.com>
2023-02-01 15:09:00 +0530

    created grid, styling and theming, navigation , passing data via constructor,named routes

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyLarge : const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyMedium : const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          titleLarge: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ),
+      home:const CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.routeName:(ctx)=>CategoryMealsScreen()
+      },
+    );
+  }
+}
